846be4af383cc0b8d9284a99cc5cfb31d2e0ff1f
Dan Dumitriu <dandumitriu33@gmail.com>
2022-11-01 09:01:31 +0200

    Add CreateBook controller function

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -34,3 +34,13 @@ func GetBookById(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func CreateBook(w http.ResponseWriter, r *http.Request){
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
