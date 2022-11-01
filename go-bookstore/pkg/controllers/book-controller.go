55b186bbb3005b8fd29fee024073581d397aa0e3
Dan Dumitriu <dandumitriu33@gmail.com>
2022-11-01 08:57:25 +0200

    Add controller get all books and one book by id functions

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -1,1 +1,36 @@
 package controllers
+
+import (
+	"encoding/json"
+	"go-bookstore/pkg/models"
+	"go-bookstore/pkg/utils"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/common/log"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		log.Errorf("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
