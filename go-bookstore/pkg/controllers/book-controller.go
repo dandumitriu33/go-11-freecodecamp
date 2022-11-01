36cd6dedcd7943a636204c6cacd7f16bc33d34fd
Dan Dumitriu <dandumitriu33@gmail.com>
2022-11-01 09:06:10 +0200

    Add Delete Book controller function

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -13,7 +13,7 @@ import (
 
 var NewBook models.Book
 
-func GetBooks(w http.ResponseWriter, r *http.Request){
+func GetBooks(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
 	res, _ := json.Marshal(newBooks)
 	w.Header().Set("Content-Type", "application/json")
@@ -21,12 +21,12 @@ func GetBooks(w http.ResponseWriter, r *http.Request){
 	w.Write(res)
 }
 
-func GetBookById(w http.ResponseWriter, r *http.Request){
+func GetBookById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
-		log.Errorf("error while parsing")
+		log.Errorf("error while parsing %v", err)
 	}
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
@@ -35,11 +35,25 @@ func GetBookById(w http.ResponseWriter, r *http.Request){
 	w.Write(res)
 }
 
-func CreateBook(w http.ResponseWriter, r *http.Request){
+func CreateBook(w http.ResponseWriter, r *http.Request) {
 	CreateBook := &models.Book{}
 	utils.ParseBody(r, CreateBook)
 	b := CreateBook.CreateBook()
 	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		log.Errorf("error while parsing %v", err)
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
