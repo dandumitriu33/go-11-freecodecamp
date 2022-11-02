64f9eb296f5b1bd001c72d91adf11e12ceafc6cc
Dan Dumitriu <dandumitriu33@gmail.com>
2022-11-02 09:13:15 +0200

    Add token replacement indicator - Slack protection on github

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -6,13 +6,16 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/shomali11/slacker"
 )
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4322258483633-4295116525767-J3C7MGCO8kl8tr1C56gm44og")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A0493JLHUBU-4309634223698-95767e4fc6eab983449aaf7c18f4e1287007d98daeb3c332b72898a7c4902d7c")
+	os.Setenv("SLACK_BOT_TOKEN", "<insert bot token>")
+	os.Setenv("SLACK_APP_TOKEN", "<insert app token>")
+	fmt.Println("BT: ", os.Getenv("SLACK_BOT_TOKEN"))
+	fmt.Println("AT: ", os.Getenv("SLACK_APP_TOKEN"))
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
@@ -20,17 +23,16 @@ func main() {
 
 	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "yob calculator",
-		Examples:     []string{"my yob is 2002"},
+		Examples:    []string{"my yob is 2002"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
 			if err != nil {
 				log.Println(err)
 			}
-			// t := time.Now().Date()
-			// currentYear := t.Year()
-			// age := currentYear - yob
-			age := 2022 - yob
+			t := time.Now()
+			currentYear := t.Year()
+			age := currentYear - yob
 
 			r := fmt.Sprintf("age is %d", age)
 			response.Reply(r)
