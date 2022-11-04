ec6a14874c0b4c75e7305e7838766c952c4e310a
Dan Dumitriu <dandumitriu33@gmail.com>
2022-11-04 09:12:22 +0200

    Add JSON notation on struct Lead

--- go-fiber-crm/lead/lead.go
+++ go-fiber-crm/lead/lead.go
@@ -1,28 +1,29 @@
 package lead
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/gofiber/fiber"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"go-fiber-crm/database"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Lead struct {
 	gorm.Model
-	Name    string
-	Company string
-	Email   string
-	Phone   int
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
 }
 
-func GetLeads(c *fiber.Ctx){
+func GetLeads(c *fiber.Ctx) {
 	db := database.DBConn
 	var leads []Lead
 	db.Find(&leads)
 	c.JSON(leads)
 }
 
-func GetLead(c *fiber.Ctx){
+func GetLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
 	var lead Lead
@@ -30,7 +31,7 @@ func GetLead(c *fiber.Ctx){
 	c.JSON(lead)
 }
 
-func NewLead(c *fiber.Ctx){
+func NewLead(c *fiber.Ctx) {
 	db := database.DBConn
 	lead := new(Lead)
 	if err := c.BodyParser(lead); err != nil {
@@ -40,7 +41,7 @@ func NewLead(c *fiber.Ctx){
 	c.JSON(lead)
 }
 
-func DeleteLead(c *fiber.Ctx){
+func DeleteLead(c *fiber.Ctx) {
 	id := c.Params("id")
 	db := database.DBConn
 	var lead Lead
